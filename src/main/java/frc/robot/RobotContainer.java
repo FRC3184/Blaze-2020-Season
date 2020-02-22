db19c490887d3ca676cf2106a01f5ef3710a7df5
DONATION <team3184@gmail.com>
2020-02-22 12:54:23 -0600

    Added the back end for Ramsete

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -9,6 +9,17 @@ package frc.robot;
 
 import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj.controller.PIDController;
+import edu.wpi.first.wpilibj.controller.RamseteController;
+import edu.wpi.first.wpilibj.controller.SimpleMotorFeedforward;
+import edu.wpi.first.wpilibj.geometry.Pose2d;
+import edu.wpi.first.wpilibj.geometry.Rotation2d;
+import edu.wpi.first.wpilibj.geometry.Translation2d;
+import edu.wpi.first.wpilibj.trajectory.Trajectory;
+import edu.wpi.first.wpilibj.trajectory.TrajectoryConfig;
+import edu.wpi.first.wpilibj.trajectory.TrajectoryGenerator;
+import edu.wpi.first.wpilibj.trajectory.constraint.DifferentialDriveVoltageConstraint;
+import edu.wpi.first.wpilibj2.command.RamseteCommand;
 import frc.robot.commands.DrivetrainCommand;
 import frc.robot.commands.ExampleCommand;
 import frc.robot.commands.IntakeCommand;
@@ -16,6 +27,8 @@ import frc.robot.subsystems.DrivetrainSubsystem;
 import frc.robot.subsystems.ExampleSubsystem;
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.subsystems.IntakeSubsystem;
+
+import java.util.List;
 
 /**
  * This class is where the bulk of the robot should be declared.  Since Command-based is a
@@ -62,8 +75,59 @@ public class RobotContainer {
      *
      * @return the command to run in autonomous
      */
-    public Command getAutonomousCommand() {
-        // An ExampleCommand will run in autonomous
-        return m_autoCommand;
+    public Command getAutonomousCommand(){
+
+        // Create a voltage constraint to ensure we don't accelerate too fast
+        var autoVoltageConstraint =
+                new DifferentialDriveVoltageConstraint(
+                        new SimpleMotorFeedforward(Constants.ksVolts,
+                                Constants.kvVoltSecondsPerMeter,
+                                Constants.kaVoltSecondsSquaredPerMeter),
+                        Constants.kDriveKinematics,
+                        10);
+
+        // Create config for trajectory
+        TrajectoryConfig config =
+                new TrajectoryConfig(Constants.kMaxSpeedMetersPerSecond,
+                        Constants.kMaxAccelerationMetersPerSecondSquared)
+                        // Add kinematics to ensure max speed is actually obeyed
+                        .setKinematics(Constants.kDriveKinematics)
+                        // Apply the voltage constraint
+                        .addConstraint(autoVoltageConstraint);
+
+        // An example trajectory to follow.  All units in meters.
+        Trajectory exampleTrajectory = TrajectoryGenerator.generateTrajectory(
+                // Start at the origin facing the +X direction
+                new Pose2d(0, 0, new Rotation2d(0)),
+                // Pass through these two interior waypoints, making an 's' curve path
+                List.of(
+                        new Translation2d(1, 1),
+                        new Translation2d(2, -1)
+                ),
+                // End 3 meters straight ahead of where we started, facing forward
+                new Pose2d(3, 0, new Rotation2d(0)),
+                // Pass config
+                config
+        );
+
+        RamseteCommand ramseteCommand = new RamseteCommand(
+                exampleTrajectory,
+                m_drivetrainSubsystem::getPose,
+                new RamseteController(Constants.kRamseteB, Constants.kRamseteZeta),
+                new SimpleMotorFeedforward(Constants.ksVolts,
+                        Constants.kvVoltSecondsPerMeter,
+                        Constants.kaVoltSecondsSquaredPerMeter),
+                Constants.kDriveKinematics,
+                m_drivetrainSubsystem::getWheelSpeeds,
+                new PIDController(Constants.kPDriveVel, 0, 0),
+                new PIDController(Constants.kPDriveVel, 0, 0),
+                // RamseteCommand passes volts to the callback
+                m_drivetrainSubsystem::tankDriveVolts,
+                m_drivetrainSubsystem
+        );
+
+        // Run path following command, then stop at the end.
+        return ramseteCommand.andThen(() -> m_drivetrainSubsystem.tankDriveVolts(0, 0));
     }
 }
+
--- src/main/java/frc/robot/subsystems/DrivetrainSubsystem.java
+++ src/main/java/frc/robot/subsystems/DrivetrainSubsystem.java
@@ -7,17 +7,24 @@
 
 package frc.robot.subsystems;
 
+import com.kauailabs.navx.frc.AHRS;
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkMaxLowLevel;
+import edu.wpi.first.wpilibj.Encoder;
+import edu.wpi.first.wpilibj.SPI;
 import edu.wpi.first.wpilibj.SpeedControllerGroup;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import edu.wpi.first.wpilibj.geometry.Pose2d;
+import edu.wpi.first.wpilibj.geometry.Rotation2d;
+import edu.wpi.first.wpilibj.kinematics.DifferentialDriveOdometry;
+import edu.wpi.first.wpilibj.kinematics.DifferentialDriveWheelSpeeds;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 
 public class DrivetrainSubsystem extends SubsystemBase {
 
-    private SpeedControllerGroup m_leftMotors;
-    private SpeedControllerGroup m_rightMotors;
+    private SpeedControllerGroup leftMotors;
+    private SpeedControllerGroup rightMotors;
 
     private CANSparkMax rearLeftDrive;
     private CANSparkMax rearRightDrive;
@@ -26,21 +33,44 @@ public class DrivetrainSubsystem extends SubsystemBase {
 
     private DifferentialDrive drive;
 
+    private AHRS ahrs;
+
+    // Odometry class for tracking robot pose
+    private final DifferentialDriveOdometry odometry;
+
+    static final double countsPerRev = 42;
+    static final double gearReduction = (50/14)*(48/16);
+    static final double wheelCircumfrence = 2*0.1524*Math.PI;
+    static final double countsPerMeter = (countsPerRev*gearReduction)/wheelCircumfrence;
+
     public DrivetrainSubsystem() {
         rearLeftDrive = new CANSparkMax(Constants.rearLeftDrivePort, CANSparkMaxLowLevel.MotorType.kBrushless);
         rearRightDrive = new CANSparkMax(Constants.rearRightDrivePort, CANSparkMaxLowLevel.MotorType.kBrushless);
         frontLeftDrive = new CANSparkMax(Constants.frontLeftDrivePort, CANSparkMaxLowLevel.MotorType.kBrushless);
         frontRightDrive = new CANSparkMax(Constants.frontRightDrivePort, CANSparkMaxLowLevel.MotorType.kBrushless);
 
-        m_leftMotors = new SpeedControllerGroup(rearLeftDrive, frontLeftDrive);
-        m_rightMotors = new SpeedControllerGroup(rearRightDrive, frontRightDrive);
+        rearLeftDrive.getEncoder().setVelocityConversionFactor((Math.PI*2*0.152)/60);
+        rearRightDrive.getEncoder().setVelocityConversionFactor((Math.PI*2*0.152)/60);
 
-        drive = new DifferentialDrive(m_leftMotors, m_rightMotors);
+        leftMotors = new SpeedControllerGroup(rearLeftDrive, frontLeftDrive);
+        rightMotors = new SpeedControllerGroup(rearRightDrive, frontRightDrive);
+
+        drive = new DifferentialDrive(leftMotors, rightMotors);
+
+        ahrs = new AHRS(SPI.Port.kMXP);
+        
+        resetEncoders();
+        odometry = new DifferentialDriveOdometry(Rotation2d.fromDegrees(getHeading()));
+
     }
 
     @Override
     public void periodic() {
         // This method will be called once per scheduler run
+
+        // Update the odometry in the periodic block
+        odometry.update(Rotation2d.fromDegrees(getHeading()), getLeftEncoderDistance(),
+                getRightEncoderDistance());
     }
 
     public void arcadeDrive(double power, double turn) {
@@ -49,6 +79,18 @@ public class DrivetrainSubsystem extends SubsystemBase {
 
     public void scaledArcadeDrive(double power, double scaledMaxPower, double turn, double scaledMaxTurn) {
         drive.arcadeDrive(powerScaler(power, scaledMaxPower), powerScaler(turn, scaledMaxTurn));
+    }
+
+    /**
+     * Controls the left and right sides of the drive directly with voltages.
+     *
+     * @param leftVolts  the commanded left output
+     * @param rightVolts the commanded right output
+     */
+    public void tankDriveVolts(double leftVolts, double rightVolts) {
+        leftMotors.setVoltage(leftVolts);
+        rightMotors.setVoltage(-rightVolts);
+        drive.feed();
     }
 
     public double powerScaler(double input, double maxPower) {
@@ -62,4 +104,112 @@ public class DrivetrainSubsystem extends SubsystemBase {
     private double scaler(double input, double inputMin, double inputMax, double outputMin, double outputMax) {
         return outputMin + (input - inputMin) * (outputMax - outputMin) / (inputMax - inputMin);
     }
+
+    /**
+     * Returns the currently-estimated pose of the robot.
+     *
+     * @return The pose.
+     */
+    public Pose2d getPose() {
+        return odometry.getPoseMeters();
+    }
+
+    /**
+     * Returns the current wheel speeds of the robot.
+     *
+     * @return The current wheel speeds.
+     */
+    public DifferentialDriveWheelSpeeds getWheelSpeeds() {
+        return new DifferentialDriveWheelSpeeds(rearLeftDrive.getEncoder().getVelocity(), rearRightDrive.getEncoder().getVelocity());
+     }
+
+    /**
+     * Resets the odometry to the specified pose.
+     *
+     * @param pose The pose to which to set the odometry.
+     */
+    public void resetOdometry(Pose2d pose) {
+        resetEncoders();
+        odometry.resetPosition(pose, Rotation2d.fromDegrees(getHeading()));
+    }
+
+
+    /**
+     * Resets the drive encoders to currently read a position of 0.
+     */
+    public void resetEncoders() {
+        rearLeftDrive.getEncoder().setPosition(0);
+        rearRightDrive.getEncoder().setPosition(0);
+        frontLeftDrive.getEncoder().setPosition(0);
+        frontRightDrive.getEncoder().setPosition(0);
+    }
+
+    /**
+     * Gets the average distance of the two encoders.
+     *
+     * @return the average of the two encoder readings
+     */
+    public double getAverageEncoderDistance() {
+        return (getLeftEncoderDistance() + getRightEncoderDistance()) / 2.0;
+    }
+
+
+    /**
+     * Sets the max output of the drive.  Useful for scaling the drive to drive more slowly.
+     *
+     * @param maxOutput the maximum output to which the drive will be constrained
+     */
+    public void setMaxOutput(double maxOutput) {
+        drive.setMaxOutput(maxOutput);
+    }
+
+    /**
+     * Zeroes the heading of the robot.
+     */
+    public void zeroHeading() {
+        ahrs.reset();
+    }
+
+    /**
+     * Returns the heading of the robot.
+     *
+     * @return the robot's heading in degrees, from -180 to 180
+     */
+    public double getHeading() {
+        return Math.IEEEremainder(ahrs.getAngle(), 360) * (Constants.gyroReversed ? -1.0 : 1.0);
+    }
+
+    /**
+     * Returns the turn rate of the robot.
+     *
+     * @return The turn rate of the robot, in degrees per second
+     */
+    public double getTurnRate() {
+        return ahrs.getRate() * (Constants.gyroReversed ? -1.0 : 1.0);
+    }
+
+    public double getLeftEncoderRate(){
+        return (rearLeftDrive.getEncoder().getVelocity());
+    }
+
+    public double getRightEncoderRate(){
+        return (rearRightDrive.getEncoder().getVelocity());
+    }
+
+    public double getLeftEncoderTicks(){
+        return (((rearLeftDrive.getEncoder().getPosition())+(frontLeftDrive.getEncoder().getPosition()))/2);
+    }
+
+    public double getRightEncoderTicks(){
+        return (((rearRightDrive.getEncoder().getPosition())+(frontRightDrive.getEncoder().getPosition()))/2);
+    }
+
+    public double getLeftEncoderDistance(){
+        return getLeftEncoderTicks()*countsPerMeter;
+    }
+
+    public double getRightEncoderDistance(){
+        return getRightEncoderTicks()*countsPerMeter;
+    }
+
 }
